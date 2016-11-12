89aaa4c6ced0a1089bbe13719cb2449990168d7e
root <root@ip-172-31-26-75.us-west-2.compute.internal>
2016-11-12 18:28:25 +0000

    added index.html as home

--- our_is531_1d_site/views.py
+++ our_is531_1d_site/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello from django, try out <a href='/admin/'>/admin/</a>\n")
-
+    #return HttpResponse("Hello from django, try out <a href='/admin/'>/admin/</a>\n")
+    return render_to_response('home/index.html', context_instance=RequestContext(request))
