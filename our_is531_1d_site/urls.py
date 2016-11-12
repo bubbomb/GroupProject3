e4e206fa291844ac66f53000f2a4c856ff68ecfe
root <root@ip-172-31-26-75.us-west-2.compute.internal>
2016-11-12 22:21:52 +0000

    configure admin interface

--- our_is531_1d_site/urls.py
+++ our_is531_1d_site/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
