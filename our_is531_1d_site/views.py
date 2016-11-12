4e5aa93a3665083793db633a31c46c646695608a
root <root@ip-172-31-26-75.us-west-2.compute.internal>
2016-11-12 20:41:11 +0000

    configure admin interface

--- our_is531_1d_site/views.py
+++ our_is531_1d_site/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def home(request):
     #return HttpResponse("Hello from django, try out <a href='/admin/'>/admin/</a>\n")
