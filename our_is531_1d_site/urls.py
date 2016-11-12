407f3ad5db5d9bbc0674cc42593488cf95f0cda8
root <root@ip-172-31-26-75.us-west-2.compute.internal>
2016-11-12 21:51:46 +0000

    More of the same

--- our_is531_1d_site/urls.py
+++ our_is531_1d_site/urls.py
@@ -21,5 +21,6 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 #    url(r'^$', 'our_is531_1d_site.views.home', name='home'),
-     url(r'^', TemplateView.as_view(template_name='index.html'), name='home'),
+#     url(r'^', TemplateView.as_view(template_name='index.html'), name='home'),
+(r"^$", direct_to_template, {"template": "index.html"})
 ]
