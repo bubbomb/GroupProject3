1118ecb2099c359b457bbd0b0af909a936fecae1
root <root@ip-172-31-26-75.us-west-2.compute.internal>
2016-11-12 22:19:30 +0000

    configure admin interface

--- our_is531_1d_site/urls.py
+++ our_is531_1d_site/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', 'our_is531_1d_site.views.home', name='home'),
-#     url(r'^', TemplateView.as_view(template_name='index.html'), name='home'),
+#    url(r'^$', 'our_is531_1d_site.views.home', name='home'),
+     url(r'^', TemplateView.as_view(template_name='index.html'), name='home'),
 #    url(r"^$", direct_to_template, {"template": "index.html"})
 ]
