cb03a4dd1f36f6e3173a5be880b090504cca1909
Brayden Connole <Brayden.Connole@gmail.com>
2016-11-21 23:55:44 -0700

    added the models and a data page

--- home/models.py
+++ home/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+class employees(models.Model):
+    fname = models.CharField(max_length = 150)
+    lname = models.CharField(max_length = 150)
+    current_office = models.CharField(max_length = 150)
+    phone  = models.CharField(max_length = 15)
+
+class manufacturers(models.Model):
+    name = models.CharField(max_length = 150)
+
+class notes(models.Model):
+    note = models.TextField()
+    asset = models.ForeignKey(
+        'assets',
+        on_delete=models.CASCADE,
+    )
+
+class assets(models.Model):
+    org_tag = models.CharField(max_length=10)
+    manufacturer = models.ForeignKey(
+        'manufacturers',
+        on_delete=models.CASCADE,
+    )
+    employee = models.ForeignKey(
+        'employees',
+        on_delete=models.CASCADE,
+    )
+    part_number = models.CharField(max_length=254)
+    description = models.TextField()
+    date_implemented = models.DateField()
+
+    MANUFACTURER_CHOICES = (
+        # Acer, 'Acer',
+        # Alien, 'Alien',
+        # Apple, 'Apple',
+        # Asus, 'Asus',
+        # Dell, 'Dell',
+        # Google, 'Google',
+        # HP, 'HP',
+        # Lenovo, 'Lenovo',
+        # LG, 'LG',
+        # Microsoft, 'Microsoft',
+        # Motorola, 'Motorola',
+        # Nexus, 'Nexus',
+        # Nokia, 'Nokia',
+        # Razer, 'Razer',
+        # Samsung, 'Samsung',
+        # Sony, 'Sony',
+        # Toshiba, 'Toshiba',
+        # Other, 'Other'
+
+    )
--- home/views.py
+++ home/views.py
@@ -7,3 +7,7 @@ def index(request):
 
 
 	return render(request, 'index.html')
+
+def data(request):
+
+	return render(request, 'data.html')
